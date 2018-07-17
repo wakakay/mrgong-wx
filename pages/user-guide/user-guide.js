d22f86dfdfb71cdfb33bce498b1ca9dd383e00f1
[yuling.zhang] <[yuling.zhang@runwise.co]>
2018-07-17 18:24:25 +0800

    ‘help' -zyl

---
+++ pages/user-guide/user-guide.js
@@ -0,0 +1,10 @@
+Page({
+    data: {
+        isChang:true,
+        imgUrls: [
+            '../../assets/images/guide01.png',
+            '../../assets/images/guide02.png',
+            '../../assets/images/guide03.png',
+        ],
+    }
+})
